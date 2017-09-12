600a4e151aa97a7b9b894b6f1212286257eefdee
黑羽 <heiyu@shelpe.com>
2017-09-12 15:34:54 +0800

    update the config file

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 
 gulp.task('less', function() {
-    gulp.src('**/index.less')
+    gulp.src('**/*.less')
         .pipe(less())
         .pipe(gulp.dest(function(f) {
             return f.base;
@@ -11,5 +11,5 @@ gulp.task('less', function() {
 });
 
 gulp.task('default', ['less'], function() {
-    gulp.watch('**/index.less', ['less']);
+    gulp.watch('**/*.less', ['less']);
 })
